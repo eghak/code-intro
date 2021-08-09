604169305863457c89dbf1b39160776d0bd452b7
Egha Kusuma <eghakusuma1@gmail.com>
2021-08-09 09:46:36 -0700

    lecture complete

---
+++ code.js
@@ -0,0 +1,36 @@
+console.log('Hello, World') 
+
+const firstName = 'Egha'
+
+let age = 34
+
+console.log(firstName, age)
+
+let isCool = true
+
+console.log(isCool)
+
+isCool = false
+
+console.log(isCool)
+
+let height
+
+console.log(height)
+
+let x = 4
+let y = 10
+let z = 10 + 4
+let sum = x + y
+
+// console.log(x, y, z)
+
+let str = `String`
+
+console.log(str)
+
+
+
+
+
+
